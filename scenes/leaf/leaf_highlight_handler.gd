4e4ac20b9e4a79bffc4452022dc0aa9dd45327d7
Matthew <matveykerov@gmail.com>
2024-10-18 18:45:17 +0300

    Added transparency to leaf highlighting

--- scenes/leaf/leaf_highlight_handler.gd
+++ scenes/leaf/leaf_highlight_handler.gd
@@ -1,11 +1,14 @@
 extends Node
 
 @export var leaf_material : StandardMaterial3D = null
-@export var highlight_color : Color = Color(1.0, 1.0, 1.0)
+@export var highlight_color : Color = Color(1.0, 1.0, 1.0, 1.0)
 @export var highlight_fade_duration = 0.3
 @export var highlight_linger_duration = 0.1
 
+var highlight_target_alpha : float = 1.0
+
 func _ready():
+	highlight_target_alpha = highlight_color.a
 	highlight_color.a = 0.0
 
 func _physics_process(delta):
@@ -19,5 +22,5 @@ func _highlight():
 	var highlight = leaf_material.next_pass as StandardMaterial3D
 	var color = highlight.albedo_color
 
-	tween.tween_property(self, "highlight_color", Color(highlight_color.r, highlight_color.g, highlight_color.b, 1.0), highlight_fade_duration)
+	tween.tween_property(self, "highlight_color", Color(highlight_color.r, highlight_color.g, highlight_color.b, highlight_target_alpha), highlight_fade_duration)
 	tween.tween_property(self, "highlight_color", Color(highlight_color.r, highlight_color.g, highlight_color.b, 0.0), highlight_fade_duration).set_delay(highlight_fade_duration + highlight_linger_duration)
