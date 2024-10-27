3f86d2052cafa211650e38b1f2dcb529acb7994c
Matthew <matveykerov@gmail.com>
2024-10-27 05:29:20 +0300

    Currency formatting

--- project/scenes/cash/cash_manager.gd
+++ project/scenes/cash/cash_manager.gd
@@ -3,7 +3,7 @@ extends Node
 @export var base_cash_reward : float = 0.1
 @export var buffer_clean_time : float = 2.0
 
-var cash : int = 0
+var cash : float = 0
 
 var cash_buffer : float = 0
 var timer : Timer = Timer.new()
@@ -29,11 +29,21 @@ func _grant_cash(amount : float = base_cash_reward) -> void:
 		timer.start()
 
 func _on_buffer_timeout():
-	var amount = round(cash_buffer)
+	var amount : float = cash_buffer
 	cash += amount
 	cash_buffer = 0
 
 	on_cash_rewarded.emit(amount)
 
-func _reward_leaf_cleaning(leaf_amount : int):
-	_grant_cash(round(leaf_amount * base_cash_reward))
+func _reward_leaf_cleaning(leaf_amount : float):
+	_grant_cash(leaf_amount * base_cash_reward)
+
+func format_currency(number : float) -> String:
+	
+	# Place the decimal separator
+	var txt_numb = "%.2f" % number
+	
+	# Place the thousands separator
+	for idx in range(txt_numb.find(".") - 3, 0, -3):
+		txt_numb = txt_numb.insert(idx, ",")
+	return(txt_numb)
