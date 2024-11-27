21e749557e03ca5c4c16aab135a9cf206ea3c0d8
Matthew <matveykerov@gmail.com>
2024-11-27 03:01:33 +0300

    Minor fixes

--- project/scenes/ui/pause_menu/scrolling_background.gd
+++ project/scenes/ui/pause_menu/scrolling_background.gd
@@ -3,7 +3,7 @@ extends TextureRect
 @export var speed : float = 0.25
 
 func _process(delta):
-	position += Vector2.RIGHT * speed
+	position += Vector2.RIGHT * speed / 0.016 * delta
 
 	if (position >= Vector2.RIGHT * 0.0):
 		position = Vector2(-1280.0, 0.0)
