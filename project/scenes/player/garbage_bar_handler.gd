16f288805482d3d42c4a6261b5f1468d18043f34
Matthew <matveykerov@gmail.com>
2024-11-30 05:50:38 +0300

    Church leves first pass

--- project/scenes/player/garbage_bar_handler.gd
+++ project/scenes/player/garbage_bar_handler.gd
@@ -25,10 +25,19 @@ func _is_full() -> bool:
     return current_fill >= capacity
 
 func _discard_bag(player : Player) -> void:
+    var jump_multiplier : float = 1.0
+    var sprint_multiplier : float = 1.0
+
+    if (!player.is_on_floor()):
+        jump_multiplier = 1.5
+
+    if (player.wish_sprint):
+        sprint_multiplier = 1.2
+
     var bag = garbage_bag_scene.instantiate() as GarbageBag
     Game.game_instance.level.add_child(bag)
     bag.global_position = player.camera_origin.global_position + Vector3.DOWN * 0.3
-    bag.apply_central_impulse(-player.camera.global_transform.basis.z * throw_strength * player.velocity_component.speed_multiplier)
+    bag.apply_central_impulse(-player.camera.global_transform.basis.z * throw_strength * player.velocity_component.speed_multiplier * jump_multiplier * sprint_multiplier)
     bag.delay = 0.0
     bag.cleaning_ticks = 999.0
     bag.cleaning_radius *= player.velocity_component.speed_multiplier
