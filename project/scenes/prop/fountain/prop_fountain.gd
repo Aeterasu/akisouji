67d79af65d07374c1c9bcd1dc328d3ed20cf8862
Matthew <matveykerov@gmail.com>
2024-12-09 11:46:55 +0300

    Added floating leaves into the fountain

--- project/scenes/prop/fountain/prop_fountain.gd
+++ project/scenes/prop/fountain/prop_fountain.gd
@@ -3,6 +3,9 @@ extends Node3D
 @export var water_mesh : MeshInstance3D = null
 @export var water_speed : float = 1.0 
 
+@export var floating_leaves : Node3D = null
+@export var leaves_speed : float = 0.1
+
 var material : StandardMaterial3D = null
 
 func _ready() -> void:
@@ -10,3 +13,5 @@ func _ready() -> void:
 
 func _physics_process(delta):
 	material.uv1_offset += Vector3(0.15, 1.0, 0.0) * water_speed * delta
+
+	floating_leaves.rotation_degrees += Vector3.UP * delta * leaves_speed
