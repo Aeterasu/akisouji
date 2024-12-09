c334a1f76627ae67ffa6cfc120042abc230fee01
Matthew <matveykerov@gmail.com>
2024-12-09 11:37:47 +0300

    Re-added timer to UI

--- project/scenes/ui/hud/ui_timer.gd
+++ project/scenes/ui/hud/ui_timer.gd
@@ -1,6 +1,7 @@
 class_name UITimer extends Control
 
 @export var label : Label = null
+@export var timer_label : Label = null
 @export var progress_bar : TextureProgressBar
 
 @export var grade : Grade = null
@@ -12,5 +13,7 @@ func _process(delta):
     label.text = tr("COMPLETION_SCORE") + ": " + str(Game.game_instance.ranking_manager._get_current_score()).pad_zeros(9)
 
     var rank = Game.game_instance.ranking_manager.get_current_rank()
+
+    timer_label.text = tr("TIME") + ": " + Game.game_instance.ranking_manager.get_formatted_time_elapsed()
 
     grade.grade = rank
