d734aa42fdf6780fa0f954ef717da8246437d933
Matthew <matveykerov@gmail.com>
2024-12-11 01:15:39 +0300

    fixed music manager again because im a dumbass

--- project/scenes/music/music_manager.gd
+++ project/scenes/music/music_manager.gd
@@ -9,35 +9,46 @@ var menu_volume : float = 1.0
 var default_gameplay_volume : float = 0.0
 var default_menu_volume : float = 0.0
 
-var gameplay_first_time : bool = false
+var gameplay_first_time : bool = true
+
+var tween : Tween = null
 
 var currently_playing : CurrentlyPlaying = CurrentlyPlaying.MENU:
 	set(value):
 		if (value == CurrentlyPlaying.MENU):
-			var tween = create_tween()
-			tween.tween_property(self, "gameplay_volume", 0.0, 0.5)
+			#if (tween):
+				#tween.kill()
+
+			tween = create_tween()
+			tween.tween_property(self, "gameplay_volume", 0.0, 0.3)
 			tween.tween_property(self, "menu_volume", default_menu_volume, 0.5)
-			tween.tween_callback(gameplay_music.stop).set_delay(0.5)
+			tween.tween_callback(_stop_gameplay_music).set_delay(0.3)
 
 			if (!menu_music.playing):
 				menu_music.play()
 
 		elif (value == CurrentlyPlaying.GAMEPLAY):
-			var tween = create_tween()
+			#if (tween):
+				#tween.kill()
+
+			tween = create_tween()
 			tween.tween_property(self, "menu_volume", 0.0, 0.2)
 			tween.tween_callback(menu_music.stop).set_delay(0.2)
 
 			gameplay_volume = default_gameplay_volume
 
 			if (!gameplay_music.playing):
-				tween.tween_callback(gameplay_music.play).set_delay(0.4)
-				(gameplay_music.stream as AudioStreamPlaylist).shuffle = true
+				tween.tween_callback(_gameplay_music_play).set_delay(0.4)
 
 enum CurrentlyPlaying
 {
 	MENU,
 	GAMEPLAY
 }
+
+func _stop_gameplay_music() -> void:
+	if (currently_playing != CurrentlyPlaying.GAMEPLAY): 
+		gameplay_music.stop()
 
 func _ready():
 	default_gameplay_volume = db_to_linear(gameplay_music.volume_db)
@@ -49,6 +60,11 @@ func _process(delta):
 	gameplay_music.volume_db = linear_to_db(gameplay_volume)
 	menu_music.volume_db = linear_to_db(menu_volume)
 
-func _first_time_gameplay_music_play() -> void:
+func _gameplay_music_play() -> void:
 	gameplay_music.play()
-	#(gameplay_music.stream as AudioStreamPlaylist).set_deferred("shuffle", true)
+
+	if (gameplay_first_time):
+		gameplay_first_time = false
+
+		await get_tree().create_timer(0.2).timeout
+		(gameplay_music.stream as AudioStreamPlaylist).set_deferred("shuffle", true)
